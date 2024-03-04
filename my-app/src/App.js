d708744fb78dc16c3a3a54a7dc02f109650544ad
Seth Crist <sethcrist50@gmail.com>
2024-03-04 11:19:08 -0600

    finished exercise

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './App.css';
 import Square from "./components/Square"
+import square from "./components/Square";
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -19,8 +20,59 @@ const App = () => {
   //losing square
   const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
 
-  return (
+    const handleGamePlay = (currentSquare) => {
+      if(currentSquare === treasureLocation) {
+          board[currentSquare] = "💎"
+          setBoard([...board])
+          setTimeout(() => {
+          alert("Not Me Treasure");
+          }, 1000)
+      } else if (currentSquare === bombLocation){
+          board[currentSquare] = "💣"
+          setBoard([...board])
+          setTimeout(() => {
+              alert("KABOOOM")
+          },1000)
+      } else {
+          board[currentSquare] = "🌳"
+          setBoard([...board])
+      }
+    }
 
+    const playAgain = () => {
+      setTreasureLocation(Math.floor(Math.random() * board.length))
+        setBombLocation(Math.floor(Math.random() * board.length))
+
+        setBoard([
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            "?"
+        ])
+    }
+
+  return (
+    <>
+        <h1>Treasure Hunt Game</h1>
+        <div className="board">
+            {board.map((square, index) => {
+                return (
+                    <Square
+                    square={square}
+                    key={index}
+                    index={index}
+                    handleGamePlay={handleGamePlay}
+                    />
+                )
+            })}
+            <button onClick={playAgain}>Play Again</button>
+        </div>
+    </>
   );
 }
 
--- my-app/src/components/Square.jsx
+++ my-app/src/components/Square.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 
-const Square = () => {
-
-
-
+const Square = (props) => {
+const handleClick = () => {
+    props.handleGamePlay(props.index)
+}
     return (
         <>
-
+            <div className="square" onClick={handleClick}>
+                {props.square}
+            </div>
         </>
     )
 }
